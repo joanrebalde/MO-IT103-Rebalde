461b0da9720955efbe33c7a3448e57a25d1e1125
joanrebalde <lr.jrebalde@mmdc.mcl.edu.ph>
2023-04-12 08:30:09 +0800

    Commit

---
+++ src/main/java/motorphapplication/Motorphapplication.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package motorphapplication;
+
+/**
+ *
+ * @author Joan
+ */
+public class Motorphapplication {
+
+    public static void main(String[] args) {
+        Home n = new Home();
+        n.setVisible(true);
+    }
+}
